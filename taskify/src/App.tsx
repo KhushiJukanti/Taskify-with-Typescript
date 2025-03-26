f534717fe893b4e1400b450d54f277248d248784
KhushiJukanti <khushijukanti33@gmail.com>
2025-03-26 17:32:16 +0530

    add all files

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Inputfield from './components/Inputfield';
+import { Todo } from './model';
+import TodoList from './components/TodoList';
 
 // let name : string;
 // let age: number;
@@ -81,10 +83,26 @@ import Inputfield from './components/Inputfield';
 // role = [5, "5"];
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
+
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+      setTodo('')
+    }
+  };
+
+  // console.log(todos)
+
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
-      <Inputfield/>
+      <Inputfield todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
